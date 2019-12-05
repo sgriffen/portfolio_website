4efba97cc3e85e809d1e5f278af5979442f76a14
sgriffen <35568914+sgriffen@users.noreply.github.com>
2019-12-05 00:15:37 +0000

    Update general.js

--- general.js
+++ general.js
@@ -2,6 +2,6 @@ function toggle_display(id) {
 
   var tog = document.getElementById(id);
   
-  if (tog.style.display === "none") { tog.style.display = "block"; }
+  if (tog.style.display === "none") { tog.style.display = "inline-block"; }
   else { tog.style.display = "none"; }
 }
