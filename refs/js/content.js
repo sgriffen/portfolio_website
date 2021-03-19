4f65684201817f79abc317c5a4899862be1b4da1
Sean <griffensean@gmail.com>
2021-03-19 14:44:47 -0500

    Updates to meet assignment requirements
    Adding senior design as a project. Adding placeholders for 2 required essays. Other small design improvements and grammer corrections

--- refs/js/content.js
+++ refs/js/content.js
@@ -2,18 +2,38 @@ function validate_input_email(input) {
 	
 	let label = findChildById(input.parentNode, input.id + "-label");
 	
-	let val = input.value.toLowerCase();
-	if (!val.includes("@") || val.includes(".")) {
-		
+	let val = input.value;
+	let inputPattern = input.pattern;
+
+	let pattern = "^";
+	pattern += inputPattern;
+	pattern += "$";
+
+	let regExp = new RegExp(pattern);
+	if (!(regExp.test(val))) {
+
 		input.style.borderColor = "#B22222;"; //border color is FireBrick
 		label.innerHTML = "Email - user@domain.com";
 		label.style.color = "#B22222"; //text color is FireBrick
 	} else {
-		
+
 		input.style.borderColor = "";
 		label.innerHTML = "Your email";
 		label.style.color = "inherit";
 	}
+	
+//	let val = input.value.toLowerCase();
+//	if (!val.includes("@") || !val.includes(".")) {
+//		
+//		input.style.borderColor = "#B22222;"; //border color is FireBrick
+//		label.innerHTML = "Email - user@domain.com";
+//		label.style.color = "#B22222"; //text color is FireBrick
+//	} else {
+//		
+//		input.style.borderColor = "";
+//		label.innerHTML = "Your email";
+//		label.style.color = "inherit";
+//	}
 }
 
 function childExists(element, id) {
