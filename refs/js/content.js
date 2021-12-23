1a794451ca456d192f36f4c8cb1d7edbbe35966f
sgriffen <griffensean@gmail.com>
2021-12-23 15:05:00 -0600

    - Updating 'about' information to reflect graduation and degree status - Updating resume to reflect current employment and education status - Updating transcript to latest version - Updating Senior Design project description/status - Removing unnecessary documents - Numerous code cleanups - Updating submodules

--- refs/js/content.js
+++ refs/js/content.js
@@ -1,7 +1,6 @@
 function validate_input_email(input) {
-	
 	let label = findChildById(input.parentNode, input.id + "-label");
-	
+
 	let val = input.value;
 	let inputPattern = input.pattern;
 
@@ -21,38 +20,25 @@ function validate_input_email(input) {
 		label.innerHTML = "Your email";
 		label.style.color = "inherit";
 	}
-	
-//	let val = input.value.toLowerCase();
-//	if (!val.includes("@") || !val.includes(".")) {
-//		
-//		input.style.borderColor = "#B22222;"; //border color is FireBrick
-//		label.innerHTML = "Email - user@domain.com";
-//		label.style.color = "#B22222"; //text color is FireBrick
-//	} else {
-//		
-//		input.style.borderColor = "";
-//		label.innerHTML = "Your email";
-//		label.style.color = "inherit";
-//	}
 }
 
 function childExists(element, id) {
-	
 	let i;
 	let childNodes = element.childNodes;
 	for (i = 0; i < childNodes.length; i++) {
 		if (childNodes[i].id === id) { return true; }
 	}
+
 	return false;
 }
 
 
 function findChildById(element, id) {
-	
 	let i;
 	let childNodes = element.childNodes;
 	for (i = 0; i < childNodes.length; i++) {
 		if (childNodes[i].id === id) { return childNodes[i]; }
 	}
+
 	return null;
 }
